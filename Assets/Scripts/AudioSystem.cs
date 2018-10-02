f008ad49626aa21a1c6d8f1101bf11aaf63e954a
Sakuralalala <32644480+Sakuralalala@users.noreply.github.com>
2018-10-02 12:36:00 +0800

    完善剧本对话以及音乐系统

--- Assets/Scripts/AudioSystem.cs
+++ Assets/Scripts/AudioSystem.cs
@@ -6,13 +6,88 @@ using UnityEngine;
 /// </summary>
 public class AudioSystem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("音乐音量"),Range(0,1.0f)]
+    public float musicVolum = 1.0f;
+
+    [Header("音乐淡出时间"),Range(0.1f,2.0f)]
+    public float musicFadeOutTime = 1.0f;
+
+    private AudioSource musicSource = null;
+
+    private void Start()
+    {
+        musicSource = GetComponent<AudioSource>();
+
+    }
+
+    /// <summary>
+    /// 播放音效
+    /// </summary>
+    /// <param name="audioClip"></param>
+    public void Play(AudioClip audioClip)
+    {
+        StartCoroutine(play(audioClip));
+    }
+    IEnumerator play(AudioClip audioClip)
+    {
+        AudioSource _audioSource = this.gameObject.AddComponent<AudioSource>();
+
+        _audioSource.clip = audioClip;
+        _audioSource.volume = musicVolum;
+        _audioSource.Play();
+        yield return new  WaitForSeconds(audioClip.length);
+        Destroy(_audioSource);
+        yield return 0;
+    }
+
+    /// <summary>
+    /// 播放音乐
+    /// </summary>
+    /// <param name="audioClip"></param>
+    public void PlayMusic(AudioClip audioClip)
+    {
+        StartCoroutine(playMusic(audioClip));
+    }
+    IEnumerator playMusic(AudioClip audioClip)
+    {
+        if(musicSource != null)
+        {
+            //将已有的bgm淡出
+            float volum = musicVolum;
+            while (volum > 0)
+            {
+                musicSource.volume = volum;
+                volum -= Time.deltaTime * musicFadeOutTime;
+                yield return 0;
+            }
+            Destroy(musicSource);
+        }
+
+        if(audioClip == null)
+        {
+            musicSource = null;
+        }
+        else
+        {
+            musicSource = this.gameObject.AddComponent<AudioSource>();
+            musicSource.clip = audioClip;
+            musicSource.volume = musicVolum;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+
+        yield return 0;
+    }
+    
+    /// <summary>
+    /// 获取到音乐片段
+    /// </summary>
+    /// <param name="clipName"></param>
+    /// <returns></returns>
+    public AudioClip GetAudioClip(string clipName)
+    {
+        return Resources.Load<AudioClip>(clipName);
+    }
+
+
 }
--- Assets/Scripts/ScriptSystem.cs
+++ Assets/Scripts/ScriptSystem.cs
@@ -1,18 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
 /// <summary>
 /// 剧本演出系统,基类
 /// </summary>
 public class ScriptSystem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //存放对话的list
+    protected List<string> dialogues_list;
+    //当前对话的人物
+    protected string role;
+    //当前对话的内容
+    protected string role_detail;
+    //对话索引
+    protected int dialogue_index = 0;
+    //对话数量
+    protected int dialogue_count = 0;
+
+
+    /// <summary>
+    /// 播放剧本的方法
+    /// </summary>
+    public virtual void PlayScript()
+    {
+
+    }
+
+   /// <summary>
+   /// 载入对应的剧本
+   /// </summary>
+   /// <param name="name">剧本的名字</param>
+   /// <param name="nodeName">剧本父节点名字</param>
+   /// <returns></returns>
+    public List<string> GetScript(string name,string nodeName)
+    {
+        XmlDocument xmlDocument = new XmlDocument();//新建一个xml
+        dialogues_list = new List<string>();
+
+        string data = Resources.Load(name).ToString();//获取路径
+        xmlDocument.Load(data);//载入xml
+        XmlNodeList xmlNodeList = xmlDocument.SelectSingleNode(nodeName).ChildNodes;//获取指定父节点下的所有子节点
+        foreach(XmlNode xmlNode in xmlNodeList)
+        {
+            XmlElement xmlElement = (XmlElement)xmlNode;
+            //加入list之中
+            dialogues_list.Add(xmlElement.ChildNodes.Item(0).InnerText + "," + xmlElement.ChildNodes.Item(1).InnerText);
+        }
+        //获取对话的总数
+        dialogue_count = dialogues_list.Count;
+
+        return dialogues_list;
+    }
 }
